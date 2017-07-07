51b5176b3515c6530989312676b8dfd20f29bc9a
Xiping Yan <xiping.yan@intel.com>
2017-07-07 16:30:39 +0800

    delete miss file

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include "header.h"
-
-int main(int argc, char** argv)
-{
-	test("this first test cmake");
-	return 0;
-}
-	
--- src.cpp
+++
@@ -1,8 +0,0 @@
-#include "header.h"
-
-void test(std::string strtip)
-{
-	std::cout << strtip << std::endl;
-}
-
-	
