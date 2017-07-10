1ba044138d476f7d4748c310135a124bdaf39699
Xiping Yan <xiping.yan@intel.com>
2017-07-10 10:45:11 +0800

    add modify src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
 #include "header.h"
-
+#ifdef USE_MYMATH
+#include "MathFunctions.h"
+#endif
 int main(int argc, char** argv)
 {
 	test("this first test cmake");
+	
+	double inputValue = 2;
+#ifdef USE_MYMATH
+	inputValue = 2;
+	double outputValue = mysqrt(inputValue);
+	fprintf(stdout, "USE_MYMAT: The square root of %g is %g\n", inputValue, outputValue);
+#else
+	inputValue = 3;
+	double outputValue = sqrt(inputValue);
+	fprintf(stdout, "USE_LIBMATH: The square root of %g is %g\n", inputValue, outputValue);
+#endif
+
 	return 0;
 }
 	
