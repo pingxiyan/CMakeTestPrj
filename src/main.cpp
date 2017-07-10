d821a2bf8fe8137f9f6f09e548f7df7de0e9939a
xiping_mj <xiping.yan@intel.com>
2017-07-10 11:30:31 +0000

    xxxx

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,14 @@
 #ifdef USE_MYMATH
 #include "MathFunctions.h"
 #endif
+
+#ifndef _WIN32
+#include <unistd.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#endif
+
 int main(int argc, char** argv)
 {
 	test("this first test cmake");
@@ -10,11 +18,11 @@ int main(int argc, char** argv)
 #ifdef USE_MYMATH
 	inputValue = 2;
 	double outputValue = mysqrt(inputValue);
-	fprintf(stdout, "USE_MYMAT: The square root of %g is %g\n", inputValue, outputValue);
+	printf("USE_MYMAT: The square root of %g is %g\n", inputValue, outputValue);
 #else
 	inputValue = 3;
 	double outputValue = sqrt(inputValue);
-	fprintf(stdout, "USE_LIBMATH: The square root of %g is %g\n", inputValue, outputValue);
+	printf("USE_LIBMATH: The square root of %g is %g\n", inputValue, outputValue);
 #endif
 
 	return 0;
